996294d7d5774fdde196105c536a2fbfd6e68689
unprofessional <unprofessionalmadman@gmail.com>
2023-12-15 15:18:31 -0500

    Fix nickname change condition block

--- events/guild_member_update.js
+++ events/guild_member_update.js
@@ -28,37 +28,41 @@ const initializeEvents = (client) => {
 
     // Guild MUST be part of a white list
     if(guildIsSupported(oldMember.guild.id)) {
-      channel.send(`\`${oldMember.user.username}\` has changed their nickname from ${oldMember.nickname} to ${newMember.nickname}!`);
-      /**
-       * This will include some gating-logic since the bot's namechange will trigger this
-       * and if we don't capture the scenario, it can be an infinite loop....
-       */
-      // member must be on the controlledMember list
-      if(memberIsControlled(oldMember.id)) {
-        const nicknamePrefix = members.find((_member) => oldMember.id === _member.memberId).prefix;
-        // console.log('>>>>> nicknamePrefix: ', nicknamePrefix);
-        if(!nickNameIsAlreadySet(newMember.nickname, nicknamePrefix)) {
-          const guildId = oldMember.guild.id;
-          const cachedGuild = client.guilds.cache.get(guildId);
-          const cachedMember = cachedGuild.members.cache.get(oldMember.id);
-          // Max-length for nicks is 32 chars
-          const replacementNickname = `[${nicknamePrefix}] ${newMember.nickname || user.username}`.substring(0,31);
-          try {
-            await cachedMember.setNickname(replacementNickname);
+
+      // Detect username change
+      if(oldMember.user.username !== newMember.user.username) {
+        channel.send(`\`${oldMember.user.username}\` has changed their nickname from ${oldMember.nickname} to ${newMember.nickname}!`);
+        /**
+         * This will include some gating-logic since the bot's namechange will trigger this
+         * and if we don't capture the scenario, it can be an infinite loop....
+         */
+        // member must be on the controlledMember list
+        if(memberIsControlled(oldMember.id)) {
+          const nicknamePrefix = members.find((_member) => oldMember.id === _member.memberId).prefix;
+          // console.log('>>>>> nicknamePrefix: ', nicknamePrefix);
+          if(!nickNameIsAlreadySet(newMember.nickname, nicknamePrefix)) {
+            const guildId = oldMember.guild.id;
+            const cachedGuild = client.guilds.cache.get(guildId);
+            const cachedMember = cachedGuild.members.cache.get(oldMember.id);
+            // Max-length for nicks is 32 chars
+            const replacementNickname = `[${nicknamePrefix}] ${newMember.nickname || user.username}`.substring(0,31);
+            try {
+              await cachedMember.setNickname(replacementNickname);
+            }
+            catch (err) {
+              channel.send('There was a problem trying to set the nickname for this user!');
+            }
           }
-          catch (err) {
-            channel.send('There was a problem trying to set the nickname for this user!');
+          else {
+            channel.send('Nick is already set! Ignoring...');
+            console.log('>>>>> Member nickname already set! Ignoring...');
+            return;
           }
         }
         else {
-          channel.send('Nick is already set! Ignoring...');
-          console.log('>>>>> Member nickname already set! Ignoring...');
-          return;
+          channel.send('Member not in the controlled list! ignoring...');
+          console.log('>>>>> Member IS NOT in the controlled list! Ignoring...');
         }
-      }
-      else {
-        channel.send('Member not in the controlled list! ignoring...');
-        console.log('>>>>> Member IS NOT in the controlled list! Ignoring...');
       }
     }
     else {
