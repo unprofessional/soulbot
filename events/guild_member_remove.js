d2960a920cc335edfb6b1dac528e18cd0f2e5807
unprofessional <unprofessionalmadman@gmail.com>
2023-12-18 18:06:46 -0500

    Fix GuildMemberRemove event detection

--- events/guild_member_remove.js
+++ events/guild_member_remove.js
@@ -2,7 +2,7 @@ const { Events } = require('discord.js');
 
 const initializeGuildMemberRemove = (client) => {
     // "guildMemberAdd"
-    client.on(Events.GuildMemberAdd, (guildMember) => {
+    client.on(Events.GuildMemberRemove, (guildMember) => {
         const channel = client.channels.cache.get("1170400835763707946");
         channel.send(`GuildMemberRemove: ${guildMember.user.username} left the server!`);
     });
