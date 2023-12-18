2de2652d9faa00fd37d6703090ead5d6e836b1d3
unprofessional <unprofessionalmadman@gmail.com>
2023-12-17 22:41:20 -0500

    Update guild member add and remove

--- events/guild_member_add.js
+++ events/guild_member_add.js
@@ -1,39 +1,10 @@
 const { Events } = require('discord.js');
-// const {
-//   members,
-//   addMember,
-//   getMembers,
-//   // removeMember,
-//   memberIsControlled,
-//   nickNameIsAlreadySet,
-// } = require("../store/members.js");
-// const { guildIsSupported } = require('../store/guilds.js');
 
 const initializeGuildMemberAdd = (client) => {
   // "guildMemberAdd"
-  client.on(Events.GuildMemberAdd, (payload) => {
-
-    console.log('>>>>> GuildMemberAdd event fired! payload: ', payload);
-
-    // FIXME: Hard-coded for now... "personal dev server" #general
-    // const channel = client.channels.cache.get("1170400835763707946");
-    // console.log('>>>>> channel: ', channel);
-    // const user = client.members.cache.get();
-    // channel.send(`GuildMemberAdd: ${user}`)
-
-    // Guild MUST be part of a white list
-    // if(guildIsSupported(oldMember.guild.id)) { // FIXME: no access to oldMember here
-    //   console.log('>>>>> GUILD SUPPORTED: GuildMemberAdd event fired!');
-
-    //   console.log('>>>>> GUILD SUPPORTED: GuildMemberAdd event fired!');
-
-    // }
-    // else {
-    //   console.log('>>>>> Server IS NOT in the supported list! Ignoring...');
-    //   channel.send('Server IS NOT in the supported list! Ignoring...');
-    // }
-
-
+  client.on(Events.GuildMemberAdd, (guildMember) => {
+    const channel = client.channels.cache.get("1170400835763707946");
+    channel.send(`GuildMemberAdd: ${guildMember.user.username} joined the server!`);
   });
 
   return client;
--- events/guild_member_remove.js
+++ events/guild_member_remove.js
@@ -1,42 +1,13 @@
 const { Events } = require('discord.js');
-// const {
-//   members,
-//   addMember,
-//   getMembers,
-//   // removeMember,
-//   memberIsControlled,
-//   nickNameIsAlreadySet,
-// } = require("../store/members.js");
-// const { guildIsSupported } = require('../store/guilds.js');
 
-const initializeGuildMemberRemove = (client) => {
-  // "guildMemberRemove"
-  client.on(Events.GuildMemberRemove, (payload) => {
-
-    console.log('>>>>> GuildMemberRemove event fired! payload: ', payload);
-
-    // FIXME: Hard-coded for now... "personal dev server" #general
-    // const channel = client.channels.cache.get("1170400835763707946");
-    // console.log('>>>>> channel: ', channel);
-    // const user = client.members.cache.get();
-    // channel.send(`GuildMemberAdd: ${user}`)
-
-    // Guild MUST be part of a white list
-    // if(guildIsSupported(oldMember.guild.id)) { // FIXME: no access to oldMember here
-    //   console.log('>>>>> GUILD SUPPORTED: GuildMemberAdd event fired!');
-
-    //   console.log('>>>>> GUILD SUPPORTED: GuildMemberAdd event fired!');
-
-    // }
-    // else {
-    //   console.log('>>>>> Server IS NOT in the supported list! Ignoring...');
-    //   channel.send('Server IS NOT in the supported list! Ignoring...');
-    // }
-
-
+const GuildMemberRemove = (client) => {
+  // "guildMemberAdd"
+  client.on(Events.GuildMemberAdd, (guildMember) => {
+    const channel = client.channels.cache.get("1170400835763707946");
+    channel.send(`GuildMemberAdd: ${guildMember.user.username} left the server!`);
   });
 
   return client;
 };
 
-module.exports = { initializeGuildMemberRemove };
+module.exports = { GuildMemberRemove };
