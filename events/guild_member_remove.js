2979d9125892f82e0adb90c8d78e1fedf2776681
unprofessional <unprofessionalmadman@gmail.com>
2023-12-18 00:13:55 -0500

    Add command support

--- events/guild_member_remove.js
+++ events/guild_member_remove.js
@@ -4,7 +4,7 @@ const initializeGuildMemberRemove = (client) => {
   // "guildMemberAdd"
   client.on(Events.GuildMemberAdd, (guildMember) => {
     const channel = client.channels.cache.get("1170400835763707946");
-    channel.send(`GuildMemberAdd: ${guildMember.user.username} left the server!`);
+    channel.send(`GuildMemberRemove: ${guildMember.user.username} left the server!`);
   });
 
   return client;
