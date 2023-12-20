2ff1fbad56fcb33d08d41417ef265572819bfe5b
unprofessional <unprofessionalmadman@gmail.com>
2023-12-19 23:32:59 -0500

    Test canvas Twitter Card rendering

--- features/fetch_metadata.js
+++ features/fetch_metadata.js
@@ -1,5 +1,5 @@
 const { unfurl } = require('unfurl.js');
-const { createCanvas } = require('canvas');
+const { createCanvas, loadImage } = require('canvas');
 
 const fetchMetadata = async (url, message) => {
     const metadata = await unfurl(url);
@@ -12,15 +12,34 @@ const fetchMetadata = async (url, message) => {
 };
 
 const renderTwitterPost = async (metadataJson, message) => {
-    const canvas = createCanvas(200, 200);
+    const canvas = createCanvas(600, 400);
     const ctx = canvas.getContext('2d');
   
     // Draw something on the canvas
-    ctx.fillStyle = 'blue';
-    ctx.fillRect(0, 0, 200, 200);
-    ctx.fillStyle = 'white';
-    ctx.font = '30px Arial';
-    ctx.fillText('Hello, Discord!', 50, 100);
+    // ctx.fillStyle = '#8E7A76';
+    // ctx.fillRect(0, 0, 400, 200);
+    // ctx.fillStyle = 'white';
+    // ctx.font = '30px Arial';
+    // ctx.fillText('Hello, Discord!', 50, 100);
+
+    // Set background color
+    ctx.fillStyle = metadataJson.theme_color;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    // Load and draw favicon
+    const favicon = await loadImage(metadataJson.favicon);
+    ctx.drawImage(favicon, 10, 10, 32, 32); // Example position and size
+
+    // Draw text elements
+    ctx.fillStyle = 'white'; // Text color
+    ctx.font = 'bold 16px Arial';
+    ctx.fillText(metadataJson.open_graph.site_name, 50, 30); // Positioning example
+
+    // Continue drawing other elements...
+
+    // Draw main image
+    const mainImage = await loadImage(metadataJson.open_graph.images[0].url);
+    ctx.drawImage(mainImage, 50, 100, 500, 250); // Example position and size
 
     // Convert the canvas to a Buffer
     const buffer = canvas.toBuffer();
