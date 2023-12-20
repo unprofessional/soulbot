79505b68daf46cdba20c0afcc4abbc39b071cfbf
unprofessional <unprofessionalmadman@gmail.com>
2023-12-20 00:28:21 -0500

    Test canvas Twitter Card 9

--- features/fetch_metadata.js
+++ features/fetch_metadata.js
@@ -83,7 +83,7 @@ const renderTwitterPost = async (metadataJson, message) => {
     const maxWidth = 500; // Maximum width for text
     const lineHeight = 20; // Line height
     const lines = getWrappedText(ctx, metadataJson.open_graph.description, maxWidth);
-    let yPosition = 50; // Starting Y position for description text
+    let yPosition = 60; // Starting Y position for description text
     lines.forEach(line => {
         ctx.fillText(line, 50, yPosition);
         yPosition += lineHeight;
