bae1935cd890b1ee65a5c59209f536e9aa8d5fd6
unprofessional <unprofessionalmadman@gmail.com>
2023-12-19 23:57:39 -0500

    Test canvas Twitter Card 2

--- features/fetch_metadata.js
+++ features/fetch_metadata.js
@@ -3,11 +3,9 @@ const { createCanvas, loadImage } = require('canvas');
 
 const fetchMetadata = async (url, message) => {
     const metadata = await unfurl(url);
-    message.channel.send(`
-\`\`\`JSON
+    message.channel.send(`\`\`\`JSON
     ${JSON.stringify(metadata, null, 2)}
-\`\`\`
-    `);
+\`\`\``);
     return metadata;
 };
 
@@ -23,29 +21,31 @@ const renderTwitterPost = async (metadataJson, message) => {
     // ctx.fillText('Hello, Discord!', 50, 100);
 
     // Set background color
-    ctx.fillStyle = metadataJson.theme_color;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = metadataJson?.theme_color;
+    // ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillRect(0, 0, 600, 400);
 
     // Load and draw favicon
-    const favicon = await loadImage(metadataJson.favicon);
-    ctx.drawImage(favicon, 10, 10, 32, 32); // Example position and size
+    // const favicon = await loadImage(metadataJson.favicon);
+    // ctx.drawImage(favicon, 10, 10, 32, 32); // Example position and size
 
     // Draw text elements
     ctx.fillStyle = 'white'; // Text color
     ctx.font = 'bold 16px Arial';
-    ctx.fillText(metadataJson.open_graph.site_name, 50, 30); // Positioning example
+    ctx.fillText(metadataJson?.open_graph?.site_name, 50, 30); // Positioning example
 
     // Continue drawing other elements...
 
     // Draw main image
-    const mainImage = await loadImage(metadataJson.open_graph.images[0].url);
+    const mainImageUrl = metadataJson?.open_graph.images[0]?.url;
+    console.log('>>>>> mainImageUrl: ', mainImageUrl);
+    const mainImage = await loadImage(mainImageUrl);
     ctx.drawImage(mainImage, 50, 100, 500, 250); // Example position and size
 
     // Convert the canvas to a Buffer
     const buffer = canvas.toBuffer();
 
     // Create a MessageAttachment and send it
-    // const attachment = new Discord.MessageAttachment(buffer, 'image.png');
     message.channel.send({
         files: [{
             attachment: buffer,
