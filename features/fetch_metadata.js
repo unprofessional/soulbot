9c426c1d01f3f70235cbc9dfaf97c324ad62c8af
unprofessional <unprofessionalmadman@gmail.com>
2023-12-20 17:48:20 -0500

    Test canvas Twitter Card 10

--- features/fetch_metadata.js
+++ features/fetch_metadata.js
@@ -3,97 +3,139 @@ const { createCanvas, loadImage } = require('canvas');
 
 const fetchMetadata = async (url, message) => {
     const metadata = await unfurl(url);
-    message.channel.send(`\`\`\`JSON
-    ${JSON.stringify(metadata, null, 2)}
+    message.channel.send(`
+\`\`\`JSON
+${JSON.stringify(metadata, null, 2)}
 \`\`\``);
     return metadata;
 };
 
-// Text wrapping function
-// function getWrappedText(ctx, text, maxWidth) {
-//   const words = text.split(' ');
-//   const lines = [];
-//   let currentLine = words[0];
-
-//   for (let i = 1; i < words.length; i++) {
-//       const word = words[i];
-//       const width = ctx.measureText(currentLine + " " + word).width;
-//       if (width < maxWidth) {
-//           currentLine += " " + word;
-//       } else {
-//           lines.push(currentLine);
-//           currentLine = word;
-//       }
-//   }
-//   lines.push(currentLine);
-//   return lines;
-// }
-
 function getWrappedText(ctx, text, maxWidth) {
-  const lines = [];
-  const paragraphs = text.split('\n'); // Split the text into paragraphs
+    const lines = [];
+    const paragraphs = text.split('\n'); // Split the text into paragraphs
 
-  paragraphs.forEach(paragraph => {
-      if (paragraph === '') {
-          lines.push(''); // Handle blank lines (paragraph breaks)
-      } else {
-          const words = paragraph.split(' ');
-          let currentLine = words[0];
+    paragraphs.forEach(paragraph => {
+        if (paragraph === '') {
+            lines.push(''); // Handle blank lines (paragraph breaks)
+        } else {
+            const words = paragraph.split(' ');
+            let currentLine = words[0];
 
-          for (let i = 1; i < words.length; i++) {
-              const word = words[i];
-              const width = ctx.measureText(currentLine + " " + word).width;
-              if (width < maxWidth) {
-                  currentLine += " " + word;
-              } else {
-                  lines.push(currentLine);
-                  currentLine = word;
-              }
-          }
+            for (let i = 1; i < words.length; i++) {
+                const word = words[i];
+                const width = ctx.measureText(currentLine + " " + word).width;
+                if (width < maxWidth) {
+                    currentLine += " " + word;
+                } else {
+                    lines.push(currentLine);
+                    currentLine = word;
+                }
+            }
 
-          lines.push(currentLine); // Push the last line of the paragraph
-      }
-  });
+            lines.push(currentLine); // Push the last line of the paragraph
+        }
+    });
 
-  return lines;
+    return lines;
 }
 
 
 const renderTwitterPost = async (metadataJson, message) => {
-    const canvas = createCanvas(600, 400);
+    const maxCanvasWidth = 600;
+    const canvas = createCanvas(maxCanvasWidth, 400);
     const ctx = canvas.getContext('2d');
 
     // Set background color
-    ctx.fillStyle = metadataJson?.theme_color;
-    // ctx.fillRect(0, 0, canvas.width, canvas.height);
+    // ctx.fillStyle = metadataJson?.theme_color;
+    // // ctx.fillRect(0, 0, canvas.width, canvas.height);
+    // ctx.fillRect(0, 0, 600, 400);
+
+
+
+
+
+
+
+
+    // // Load and draw favicon
+    // const favicon = await loadImage(metadataJson.favicon);
+    // ctx.drawImage(favicon, 10, 10, 32, 32); // Example position and size
+
+    // // Draw text elements
+    // ctx.fillStyle = 'white'; // Text color
+    // ctx.font = 'bold 20px Arial';
+    // ctx.fillText(metadataJson?.open_graph?.title, 50, 30); // Positioning example
+
+    // // Draw description with text wrapping
+    // ctx.fillStyle = 'white'; // Text color for description
+    // ctx.font = '24px Arial';
+    // const maxWidth = 500; // Maximum width for text
+    // const lineHeight = 20; // Line height
+    // const lines = getWrappedText(ctx, metadataJson.open_graph.description, maxWidth);
+    // let yPosition = 60; // Starting Y position for description text
+    // lines.forEach(line => {
+    //     ctx.fillText(line, 50, yPosition);
+    //     yPosition += lineHeight;
+    // });
+
+    // // ctx.canvas.height = 800;
+
+    // // Draw main image
+    // const mainImageUrl = metadataJson?.open_graph?.images[0]?.url;
+    // // console.log('>>>>> mainImageUrl: ', mainImageUrl);
+    // // const mainImage = await loadImage(mainImageUrl);
+    // // ctx.drawImage(mainImage, 50, 100, 500, 250); // Example position and size
+
+
+
+
+    ctx.fillStyle = metadataJson.theme_color;
     ctx.fillRect(0, 0, 600, 400);
 
-    // Load and draw favicon
+    // // Load and draw favicon
     const favicon = await loadImage(metadataJson.favicon);
-    ctx.drawImage(favicon, 10, 10, 32, 32); // Example position and size
-
+    ctx.drawImage(favicon, 550, 10, 32, 32); // Example position and size
+    
+    let canvasHeight = 650;
+    
+    ctx.canvas.height = canvasHeight;
+    ctx.fillRect(0, 0, 600, canvasHeight);
+    
+    // Pre-process description with text wrapping
+    const maxCharLength = 250; // Maximum width for text
+    const descLines = getWrappedText(ctx, metadataJson.open_graph.description, maxCharLength);
+    console.log('>>>>> descLines.length: ', descLines.length);
+    let yPosition = 110; // Starting Y position for description text
+    
+    // New height calcs
+    const descLinesLength = descLines.length;
+    console.log('>>>>> descLines: ', descLines);
+    const calculatedCanvasHeightFromDescLines = (descLinesLength * 30) + yPosition + 20;
+    console.log('>>>>> calculatedCanvasHeightFromDescLines: ', calculatedCanvasHeightFromDescLines);
+    
+    // Re-calc canvas
+    ctx.canvas.height = calculatedCanvasHeightFromDescLines;
+    ctx.fillRect(0, 0, 600, calculatedCanvasHeightFromDescLines);
+    
     // Draw text elements
     ctx.fillStyle = 'white'; // Text color
-    ctx.font = 'bold 16px Arial';
-    ctx.fillText(metadataJson?.open_graph?.title, 50, 30); // Positioning example
-
-    // Draw description with text wrapping
+    ctx.font = 'bold 18px Arial';
+    ctx.fillText(metadataJson?.open_graph?.title, 100, 40);
+    
+    // Pre-process description with text wrapping
     ctx.fillStyle = 'white'; // Text color for description
-    ctx.font = '14px Arial';
-    const maxWidth = 500; // Maximum width for text
-    const lineHeight = 20; // Line height
-    const lines = getWrappedText(ctx, metadataJson.open_graph.description, maxWidth);
-    let yPosition = 60; // Starting Y position for description text
-    lines.forEach(line => {
-        ctx.fillText(line, 50, yPosition);
+    ctx.font = '24px Arial';
+    const lineHeight = 30; // Line height
+    descLines.forEach(line => {
+        ctx.fillText(line, 30, yPosition);
         yPosition += lineHeight;
     });
-
+    
     // Draw main image
-    const mainImageUrl = metadataJson?.open_graph?.images[0]?.url;
-    // console.log('>>>>> mainImageUrl: ', mainImageUrl);
-    // const mainImage = await loadImage(mainImageUrl);
-    // ctx.drawImage(mainImage, 50, 100, 500, 250); // Example position and size
+    const mainImage = await loadImage("https://abs.twimg.com/favicons/twitter.3.ico");
+    ctx.drawImage(mainImage, 20, 20, 50, 50); // Example position and size
+
+
 
     // Convert the canvas to a Buffer
     const buffer = canvas.toBuffer();
