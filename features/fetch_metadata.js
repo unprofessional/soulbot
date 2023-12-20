8943f7b2b99b1ec3f6d0d35b1b8e1ce495596d39
unprofessional <unprofessionalmadman@gmail.com>
2023-12-19 21:54:25 -0500

    Test furl twitter metadata fetch 4 LINT

--- features/fetch_metadata.js
+++ features/fetch_metadata.js
@@ -2,36 +2,36 @@ const { unfurl } = require('unfurl.js');
 const { createCanvas } = require('canvas');
 
 const fetchMetadata = async (url, message) => {
-  const metadata = await unfurl(url);
-  message.channel.send(JSON.stringify(metadata, null, 2));
-  return metadata;
+    const metadata = await unfurl(url);
+    message.channel.send(JSON.stringify(metadata, null, 2));
+    return metadata;
 };
 
 const renderTwitterPost = async (metadataJson, message) => {
-  const canvas = createCanvas(200, 200);
-  const ctx = canvas.getContext('2d');
+    const canvas = createCanvas(200, 200);
+    const ctx = canvas.getContext('2d');
   
-  // Draw something on the canvas
-  ctx.fillStyle = 'blue';
-  ctx.fillRect(0, 0, 200, 200);
-  ctx.fillStyle = 'white';
-  ctx.font = '30px Arial';
-  ctx.fillText('Hello, Discord!', 50, 100);
+    // Draw something on the canvas
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(0, 0, 200, 200);
+    ctx.fillStyle = 'white';
+    ctx.font = '30px Arial';
+    ctx.fillText('Hello, Discord!', 50, 100);
 
-  // Convert the canvas to a Buffer
-  const buffer = canvas.toBuffer();
+    // Convert the canvas to a Buffer
+    const buffer = canvas.toBuffer();
 
-  // Create a MessageAttachment and send it
-  // const attachment = new Discord.MessageAttachment(buffer, 'image.png');
-  message.channel.send({
-    files: [{
-        attachment: buffer,
-        name: 'image.png'
-      }]
+    // Create a MessageAttachment and send it
+    // const attachment = new Discord.MessageAttachment(buffer, 'image.png');
+    message.channel.send({
+        files: [{
+            attachment: buffer,
+            name: 'image.png'
+        }]
     });
 };
 
 module.exports = {
-  fetchMetadata,
-  renderTwitterPost,
+    fetchMetadata,
+    renderTwitterPost,
 };
