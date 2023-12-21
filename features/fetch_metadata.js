12781154e3392b0f0d90ffef08616f15d407b94a
unprofessional <unprofessionalmadman@gmail.com>
2023-12-20 22:21:16 -0500

    Test new VXApi metadata Twitter Card 4

--- features/fetch_metadata.js
+++ features/fetch_metadata.js
@@ -11,15 +11,16 @@ const fetchMetadata = async (url, message) => {
     const extractedPart = parts[1];
     const vxApiUrl = `https://api.vxtwitter.com/${extractedPart}`;
     const result = await fetch(vxApiUrl);
+    let resultJson = {};
     try {
-        const resultJson = await result.json();
+        resultJson = await result.json();
         console.log('>>>>> fetch > resultJson: ', resultJson);
     } catch (err) {
         console.log('>>>>> fetch > err: ', err);
         message.reply(`${err}`);
     }
 
-    return result;
+    return resultJson;
 };
 
 module.exports = {
