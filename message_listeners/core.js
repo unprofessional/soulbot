c8f907559bc1d49faa24a72e8d4089fe9b7908c0
unprofessional <unprofessionalmadman@gmail.com>
2023-12-21 01:12:51 -0500

    Test new VXApi metadata Twitter Card 15

--- message_listeners/core.js
+++ message_listeners/core.js
@@ -165,7 +165,7 @@ const initializeListeners = (client) => {
                 const metadata = await fetchMetadata(firstUrl, message);
 
                 if (metadata.error) {
-                    message.reply(metadata.errorMsg);
+                    message.reply(`Server 500: Failed to extract tweet (Processing error) ${metadata.errorMsg}`);
                 } else {
                     // console.log('>>>>> fetchMetadata > metadata: ', JSON.stringify(metadata, null, 2));
                     renderTwitterPost(metadata, message);
