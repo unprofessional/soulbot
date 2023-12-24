518dc91f6d4faede36f634a221cb55a79054aae4
unprofessional <unprofessionalmadman@gmail.com>
2023-12-23 20:52:19 -0500

    Twitter Card dynamic dimensions 8

--- features/twitter_canvas.js
+++ features/twitter_canvas.js
@@ -184,36 +184,34 @@ const createTwitterCanvas = async (metadataJson) => {
     if (hasImgs) {
         const mainMedia1Url = metadata.mediaUrls[0];
         const mainMedia1 = await loadImage(mainMedia1Url);
-        mainMedia1.onload = function() {
-            // Calculate the aspect ratio of the destination size
-            const destAspectRatio = mediaMaxWidth / mediaMaxHeight;
+        // Calculate the aspect ratio of the destination size
+        const destAspectRatio = mediaMaxWidth / mediaMaxHeight;
 
-            // Determine the cropping size (maintaining the destination aspect ratio)
-            let cropWidth, cropHeight;
-            if (mainMedia1.width / mainMedia1.height > destAspectRatio) {
-                // Image is wider than destination aspect ratio
-                cropHeight = mainMedia1.height;
-                cropWidth = mainMedia1.height * destAspectRatio;
-            } else {
-                // Image is taller than destination aspect ratio
-                cropWidth = mainMedia1.width;
-                cropHeight = mainMedia1.width / destAspectRatio;
-            }
+        // Determine the cropping size (maintaining the destination aspect ratio)
+        let cropWidth, cropHeight;
+        if (mainMedia1.width / mainMedia1.height > destAspectRatio) {
+            // Image is wider than destination aspect ratio
+            cropHeight = mainMedia1.height;
+            cropWidth = mainMedia1.height * destAspectRatio;
+        } else {
+            // Image is taller than destination aspect ratio
+            cropWidth = mainMedia1.width;
+            cropHeight = mainMedia1.width / destAspectRatio;
+        }
 
-            // Calculate starting point (top left corner) for cropping
-            const sx = (mainMedia1.width - cropWidth) / 2;
-            const sy = (mainMedia1.height - cropHeight) / 2;
+        // Calculate starting point (top left corner) for cropping
+        const sx = (mainMedia1.width - cropWidth) / 2;
+        const sy = (mainMedia1.height - cropHeight) / 2;
 
-            const position = calculatedCanvasHeightFromDescLines - mediaMaxHeight - 50;
+        const position = calculatedCanvasHeightFromDescLines - mediaMaxHeight - 50;
 
-            console.log('>>>>> hasImgs! drawing image...');
-            // Draw the cropped image on the canvas
-            ctx.drawImage(
-                mainMedia1,
-                sx, sy, cropWidth, cropHeight, // Source rectangle
-                20, position, mediaMaxWidth, mediaMaxHeight // Destination rectangle
-            );
-        };
+        console.log('>>>>> hasImgs! drawing image...');
+        // Draw the cropped image on the canvas
+        ctx.drawImage(
+            mainMedia1,
+            sx, sy, cropWidth, cropHeight, // Source rectangle
+            20, position, mediaMaxWidth, mediaMaxHeight // Destination rectangle
+        );
     }
 
     // Convert the canvas to a Buffer and return it
