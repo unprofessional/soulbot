384a921f77259c925f21f854b32c60edf784a621
unprofessional <unprofessionalmadman@gmail.com>
2023-12-26 23:07:41 -0500

    Attempt to handle video post scenario 3b

--- features/render_twitter_post.js
+++ features/render_twitter_post.js
@@ -10,7 +10,7 @@ const renderTwitterPost = async (metadataJson, message) => {
     console.log('>>>>> renderTwitterPost > metadataJson: ', metadataJson);
     const mediaUrls = metadataJson.mediaURLs;
     console.log('>>>>> renderTwitterPost > mediaUrls: ', mediaUrls);
-    let mediaUrlsFormatted = "";
+    // let mediaUrlsFormatted = "";
     let files = [{
         attachment: buffer,
         name: 'image.png',
@@ -31,10 +31,10 @@ const renderTwitterPost = async (metadataJson, message) => {
 
     if(hasVids) {
         mediaUrls.forEach((mediaUrl) => {
-            mediaUrlsFormatted += `${mediaUrl}\n` // TODO: fix singular dangling comma
+            // mediaUrlsFormatted += `${mediaUrl}\n` // TODO: fix singular dangling comma
             files.push({
                 attachment: mediaUrl,
-                name: 'image.png', // FIXME: Use the actual file hash + extension etc
+                name: 'video.mp4', // FIXME: Use the actual file hash + extension etc
             });
         });
     } else {
