5d6f5107905880d24180f7cb9670e6338b1b2c54
unprofessional <unprofessionalmadman@gmail.com>
2024-11-03 21:27:30 -0500

    Test fonts 17

--- features/twitter-post/twitter_canvas.js
+++ features/twitter-post/twitter_canvas.js
@@ -105,7 +105,8 @@ const createTwitterCanvas = async (metadataJson, isImage) => {
 
     function setFontBasedOnContent(ctx, text) {
         console.log('>>> setFontBasedOnContent reached!');
-        const emojiPattern = /[\u{1F600}-\u{1F64F}]/u; // Adjust pattern as needed for emojis
+        const emojiPattern = /[\u{1F300}-\u{1F5FF}\u{1F600}-\u{1F64F}\u{1F680}-\u{1F6FF}\u{1F700}-\u{1F77F}\u{1F780}-\u{1F7FF}\u{1F800}-\u{1F8FF}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FAFF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}]/u;
+
         if (emojiPattern.test(text)) {
             console.log('>>> Emoji detected!');
             ctx.textDrawingMode = "glyph";
