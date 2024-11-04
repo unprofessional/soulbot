2efc5097c62ed72460422c6c470756f7c4f40b38
unprofessional <unprofessionalmadman@gmail.com>
2024-11-04 00:54:58 -0500

    Test fonts 27

--- features/twitter-post/twitter_canvas.js
+++ features/twitter-post/twitter_canvas.js
@@ -107,10 +107,12 @@ const createTwitterCanvas = async (metadataJson, isImage) => {
         console.log('>>> setFontBasedOnContent reached!');
         const emojiPattern = /[\u{1F300}-\u{1F5FF}\u{1F600}-\u{1F64F}\u{1F680}-\u{1F6FF}\u{1F700}-\u{1F77F}\u{1F780}-\u{1F7FF}\u{1F800}-\u{1F8FF}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FAFF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}]/u;
 
+        console.log('>>> setFontBasedOnContent > text: ', text);
+
         if (emojiPattern.test(text)) {
             console.log('>>> Emoji detected!');
             ctx.textDrawingMode = "glyph";
-            ctx.font = '24px "Noto Color Emoji';
+            ctx.font = '24px Arial';
             ctx.textDrawingMode = "glyph";
         } else {
             console.log('>>> Emoji NOT detected...');
