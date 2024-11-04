4356666673884053e2e831bff5f12eda2bdef4cc
unprofessional <unprofessionalmadman@gmail.com>
2024-11-04 00:11:12 -0500

    Test fonts 23

--- features/twitter-post/twitter_canvas.js
+++ features/twitter-post/twitter_canvas.js
@@ -99,9 +99,9 @@ const createTwitterCanvas = async (metadataJson, isImage) => {
     // registerFont('/usr/share/fonts/truetype/noto/NotoColorEmoji.ttf', { family: 'Noto Color Emoji' });
 
     // Register Noto Sans CJK Regular and Bold
-    registerFont('/usr/share/fonts/opentype/noto/NotoSansCJK-VF.ttf.ttc', { family: 'Noto Sans CJK' });
+    // registerFont('/usr/share/fonts/opentype/noto/NotoSansCJK-VF.ttf.ttc', { family: 'Noto Sans CJK' });
 
-    const globalFont = '"Arial", "Noto Sans CJK"';
+    const globalFont = 'Arial';
 
     // function setFontBasedOnContent(ctx, text) {
     //     console.log('>>> setFontBasedOnContent reached!');
