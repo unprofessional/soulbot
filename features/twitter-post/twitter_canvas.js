0cb9ba238038a999f51c433d065c40f5858ed2e2
unprofessional <unprofessionalmadman@gmail.com>
2024-11-05 00:59:56 -0500

    Test font letter spacing 3

--- features/twitter-post/twitter_canvas.js
+++ features/twitter-post/twitter_canvas.js
@@ -264,7 +264,7 @@ const createTwitterCanvas = async (metadataJson, isImage) => {
                 setFontBasedOnContent(ctx, line);
             }
             // ctx.fillText(line, descXPosition, defaultYPosition);
-            drawTextWithSpacing(ctx, line, descXPosition, defaultYPosition, 2);
+            drawTextWithSpacing(ctx, line, descXPosition, defaultYPosition, 1.5);
             defaultYPosition += lineHeight;
         });
 
