bf2a2b30f84130416a65864a98c86ddd87701e1d
unprofessional <unprofessionalmadman@gmail.com>
2024-11-08 00:17:57 -0500

    Test font simplificartion

--- features/twitter-core/canvas_utils.js
+++ features/twitter-core/canvas_utils.js
@@ -43,19 +43,22 @@ function getWrappedText(ctx, text, maxWidth, hasVids) {
 function setFontBasedOnContent(ctx, text) {
     console.log('>>> setFontBasedOnContent reached!');
 
-    const emojiPattern = /[\u{1F300}-\u{1F5FF}\u{1F600}-\u{1F64F}\u{1F680}-\u{1F6FF}\u{1F700}-\u{1F77F}\u{1F780}-\u{1F7FF}\u{1F800}-\u{1F8FF}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FAFF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}\u{1F1E6}-\u{1F1FF}\u{203C}-\u{3299}]/u;
+    // const emojiPattern = /[\u{1F300}-\u{1F5FF}\u{1F600}-\u{1F64F}\u{1F680}-\u{1F6FF}\u{1F700}-\u{1F77F}\u{1F780}-\u{1F7FF}\u{1F800}-\u{1F8FF}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FAFF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}\u{1F1E6}-\u{1F1FF}\u{203C}-\u{3299}]/u;
 
     console.log('>>> setFontBasedOnContent > text: ', text);
 
-    if (emojiPattern.test(text)) {
-        console.log('>>> Emoji detected!');
-        ctx.textDrawingMode = "glyph";
-        ctx.font = '24px "Noto Color Emoji"';
-    }
-    else {
-        console.log('>>> Emoji NOT detected...');
-        ctx.font = '24px "Noto Color Emoji"';
-    }
+    ctx.textDrawingMode = "glyph";
+    ctx.font = '24px "Noto Color Emoji"';
+
+    // if (emojiPattern.test(text)) {
+    //     console.log('>>> Emoji detected!');
+    //     ctx.textDrawingMode = "glyph";
+    //     ctx.font = '24px "Noto Color Emoji"';
+    // }
+    // else {
+    //     console.log('>>> Emoji NOT detected...');
+    //     ctx.font = '24px "Noto Color Emoji"';
+    // }
 }
 
 const drawDescription = (ctx, hasImgs, hasVids, hasOnlyVideos, descLines, font, x, y, isQt) => {
