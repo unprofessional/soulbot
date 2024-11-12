aec64e6aec7686a03d9cfbe21d4d5bed0d32d45b
unprofessional <unprofessionalmadman@gmail.com>
2024-11-11 22:47:01 -0500

    Testing embeds 19

--- features/twitter-core/canvas_utils.js
+++ features/twitter-core/canvas_utils.js
@@ -57,12 +57,13 @@ function getWrappedText(ctx, text, maxWidth) {
 const drawDescription = (ctx, hasImgs, hasVids, descLines, font, x, y, isQt) => {
     console.log('>>>>> canvas_utils > drawDescription > descLines: ', descLines);
     console.log('>>>>> canvas_utils > drawDescription > descLines.length: ', descLines.length);
-    const lineHeight = 30;
+    const isQtWithMedia = isQt && (hasImgs || hasVids);
+    const lineHeight = isQtWithMedia ? 24 : 30;
     console.log('>>>>> canvas_utils > drawDescription > hasImgs || hasVids: ', hasImgs || hasVids);
     descLines.forEach(line => {
         ctx.textDrawingMode = "glyph";
         // ctx.font = isQt ? '18px "Noto Color Emoji"' : '24px "Noto Color Emoji"';
-        ctx.font = '24px "Noto Color Emoji"';
+        ctx.font = isQtWithMedia ? '18px "Noto Color Emoji"' : '24px "Noto Color Emoji"';
         if(!hasImgs && hasVids) {
             console.log('>>>>> canvas_utils > drawDescription > !hasImgs and hasVids!');
             ctx.font = '36px ' + font;
