545d31ade3d049c6a1fbd0c07b885abca175a5f9
unprofessional <unprofessionalmadman@gmail.com>
2024-11-21 19:43:22 -0500

    Attempt fix for video canvas proportions 8

--- features/twitter-post/scale_down.js
+++ features/twitter-post/scale_down.js
@@ -11,15 +11,21 @@ const scaleDownToFitAspectRatio = (
     console.log('>>>>> scaleDownToFitAspectRatio > compensatedHeight: ', compensatedHeight);
     // Calculate the effective maximum height, ensuring it is not negative
     const effectiveMaxHeight = Math.max(0, mediaMaxHeight - compensatedHeight);
+    console.log('>>>>> scaleDownToFitAspectRatio > effectiveMaxHeight: ', effectiveMaxHeight);
 
     // Calculate scaling factors for both width and height limits
     const widthScale = mediaMaxWidth / width;
     const heightScale = effectiveMaxHeight / height;
     const scaleFactor = Math.min(widthScale, heightScale);
+    console.log('>>>>> scaleDownToFitAspectRatio > widthScale: ', widthScale);
+    console.log('>>>>> scaleDownToFitAspectRatio > heightScale: ', heightScale);
+    console.log('>>>>> scaleDownToFitAspectRatio > scaleFactor: ', scaleFactor);
 
     // Scale dimensions using the calculated scaleFactor, preserving aspect ratio
     const adjustedWidth = Math.floor(width * scaleFactor);
     const adjustedHeight = Math.floor(height * scaleFactor);
+    console.log('>>>>> scaleDownToFitAspectRatio > adjustedWidth: ', adjustedWidth);
+    console.log('>>>>> scaleDownToFitAspectRatio > adjustedHeight: ', adjustedHeight);
 
     return {
         height: adjustedHeight,
