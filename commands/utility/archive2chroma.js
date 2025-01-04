4187f61326c3f84b0295c87b20645573cef4af2e
unprofessional <unprofessionalmadman@gmail.com>
2025-01-04 16:30:11 -0500

    Add archive2chroma app command 3

--- commands/utility/archive2chroma.js
+++ commands/utility/archive2chroma.js
@@ -1,11 +1,18 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { archiveHistoryToChromaDb } = require('../../features/ollama/embed');
 
+const BOT_OWNER_ID = process.env.BOT_OWNER_ID || '818606180095885332';
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('archive2chroma')
         .setDescription('Archives historical messages to ChromaDB.'),
     async execute(interaction) {
+        if (interaction.user.id !== BOT_OWNER_ID) {
+            return await interaction.reply({
+                content: 'You do not have permission to use this command.',
+                ephemeral: true,
+            });
+        }
         await interaction.deferReply();
 
         try {
