f452b2e4129b7829bdc32c68b9dbf002d7bc20f8
unprofessional <unprofessionalmadman@gmail.com>
2025-02-02 16:54:16 -0500

    Add metadata error scenario check 20

--- features/twitter-post/twitter_canvas.js
+++ features/twitter-post/twitter_canvas.js
@@ -37,7 +37,7 @@ const createTwitterCanvas = async (metadataJson, isImage) => {
         };
     }
     if(metadataJson?.qtMetadata?.error) {
-        qtMetadata = metadataJson;
+        qtMetadata = metadataJson.qtMetadata;
     }
     console.log('>>>>> createTwitterCanvas > qtMetadata: ', qtMetadata);
 
