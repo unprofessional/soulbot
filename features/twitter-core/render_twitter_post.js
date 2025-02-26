9ebf505f9a178a5500ea4439d4c85cbb5c253477
unprofessional <unprofessionalmadman@gmail.com>
2025-02-26 02:22:17 -0500

    Add debug to stripQueryParam attempt 3

--- features/twitter-core/render_twitter_post.js
+++ features/twitter-core/render_twitter_post.js
@@ -76,13 +76,13 @@ const sendWebhookProxyMsg = async (message, content, files = [], communityNoteTe
         // const twitterOrXUrlPattern = /https?:\/\/(?:twitter\.com|x\.com)\/[a-zA-Z0-9_]+\/status\/\d+/g;
 
         const urlWithQueryParams = message.content.match(twitterOrXUrlWithQueryParamPattern);
-        console.log('>>> sendWebhookProxyMsg > twitterOrXUrlWithQueryParamPattern: ', twitterOrXUrlWithQueryParamPattern);
+        console.log('>>> sendWebhookProxyMsg > urlWithQueryParams: ', urlWithQueryParams);
         const strippedUrl = stripQueryParams(urlWithQueryParams[0]);
         console.log('>>> sendWebhookProxyMsg > strippedUrl: ', strippedUrl);
 
         const trimmedContent = message.content.replace(twitterOrXUrlWithQueryParamPattern, strippedUrl);
         console.log('>>> sendWebhookProxyMsg > trimmedContent: ', trimmedContent);
-        
+
         const modifiedContent = message.content.replace(/(https:\/\/\S+)/, '<$1>');
 
         // Send the message through the webhook
