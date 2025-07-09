9abba4f99396dfa9408ff5ba6b855206e9bf48df
unprofessional <unprofessionalmadman@gmail.com>
2025-07-09 02:05:55 -0400

    REFACTOR 3

--- commands/global/rpg-tracker/join-game.js
+++ commands/global/rpg-tracker/join-game.js
@@ -1,8 +1,7 @@
 // commands/global/rpg-tracker/join-game.js
 
-const { SlashCommandBuilder, ActionRowBuilder, StringSelectMenuBuilder } = require('discord.js');
-const { getGame } = require('../../../store/services/game.service');
-const { getOrCreatePlayer } = require('../../../store/services/player.service');
+const { SlashCommandBuilder } = require('discord.js');
+const { build } = require('../../../features/rpg-tracker/components/join_game_selector');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -10,54 +9,16 @@ module.exports = {
         .setDescription('Select a public game in this server to join.'),
 
     async execute(interaction) {
-        const guildId = interaction.guildId;
-        const userId = interaction.user.id;
+        const { user, guild } = interaction;
 
-        if (!guildId) {
+        if (!guild) {
             return interaction.reply({
                 content: '⚠️ You must use this command in a server (not DMs).',
                 ephemeral: true,
             });
         }
 
-        // Ensure player and server link exists (but don't assign a game yet)
-        await getOrCreatePlayer(userId, guildId);
-
-        const games = await getGame({ guildId });
-
-        // Filter out games that are not public or were created by the user
-        const eligibleGames = games.filter(game =>
-            game.is_public && game.created_by !== userId
-        );
-
-        if (!eligibleGames.length) {
-            return interaction.reply({
-                content: [
-                    '📭 There are no joinable public games in this server right now.',
-                    '',
-                    'If you created a game, you’re already considered a player as the **Game Master**.',
-                ].join('\n'),
-                ephemeral: true,
-            });
-        }
-
-        const menu = new StringSelectMenuBuilder()
-            .setCustomId('joinGameDropdown')
-            .setPlaceholder('Select a game to join')
-            .addOptions(
-                eligibleGames.slice(0, 25).map(game => ({
-                    label: game.name.slice(0, 100),
-                    description: game.description?.slice(0, 100) || 'No description',
-                    value: game.id,
-                }))
-            );
-
-        const row = new ActionRowBuilder().addComponents(menu);
-
-        await interaction.reply({
-            content: '🎲 Choose a game you want to join:',
-            components: [row],
-            ephemeral: true,
-        });
-    }
+        const response = await build(user.id, guild.id);
+        return interaction.reply(response);
+    },
 };
