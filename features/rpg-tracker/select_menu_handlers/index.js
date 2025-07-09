19f87be2c22602dbc865a0e0ea1909c7fe50b083
unprofessional <unprofessionalmadman@gmail.com>
2025-07-09 02:08:30 -0400

    REFACTOR 3a

--- features/rpg-tracker/select_menu_handlers/index.js
+++ features/rpg-tracker/select_menu_handlers/index.js
@@ -1,7 +1,6 @@
 // features/rpg-tracker/select_menu_handlers/index.js
 
 const characterDropdown = require('./character_dropdown');
-const gameDropdown = require('./game_dropdown');
 const statDropdown = require('./stat_template_dropdown');
 const characterStatSelect = require('./character_stat_select_menu');
 const statTypeDropodown = require('./stat_type_select');
@@ -9,10 +8,17 @@ const adjustNumericStatSelectHandler = require('./adjust_numeric_stat_select');
 const publicCharacterSelect = require('./public_character_select');
 const { handle : handleSwitchCharacterSelector } = require('../components/switch_character_selector');
 const { handle: handleSwitchGameSelector } = require('../components/switch_game_selector');
+const { handle: handleJoinGameSelector } = require('../components/join_game_selector');
 
 module.exports = {
     async handleSelectMenu(interaction) {
         const { customId } = interaction;
+
+        if (customId === 'switchCharacterDropdown') return handleSwitchCharacterSelector(interaction);
+
+        if (customId === 'switchGameDropdown') return handleSwitchGameSelector(interaction);
+
+        if (customId === 'joinGameDropdown') return handleJoinGameSelector(interaction);
 
         if (
             customId === 'createCharacterDropdown' ||
@@ -20,13 +26,6 @@ module.exports = {
         ) {
             return characterDropdown.handle(interaction);
         }
-
-        if (customId === 'switchCharacterDropdown') return handleSwitchCharacterSelector(interaction);
-
-        if (customId === 'switchGameDropdown') return handleSwitchGameSelector(interaction);
-
-        // legacy support
-        if (customId === 'joinGameDropdown') return gameDropdown.handle(interaction);
 
         if (
             customId.startsWith('editStatSelect:') ||
