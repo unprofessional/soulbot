598182b59f2eda18b8c8fe86792bd61de07ddc0e
unprofessional <unprofessionalmadman@gmail.com>
2025-12-27 23:36:23 -0500

    Add egyptian and banum glyphs 1

--- features/twitter-post/canvas/fonts.js
+++ features/twitter-post/canvas/fonts.js
@@ -4,6 +4,8 @@ const { registerFont } = require('canvas');
 const FONT_PATHS = [
     ['/truetype/noto/NotoColorEmoji.ttf', 'Noto Color Emoji'],
     ['/truetype/noto/NotoSansMath-Regular.ttf', 'Noto Sans Math'],
+    ['/truetype/noto/NotoSansBamum-Regular.ttf', 'Noto Sans Bamum'],
+    ['/truetype/noto/NotoSansEgyptianHieroglyphs-Regular.ttf', 'Noto Sans Egyptian Hieroglyphs'],
     ['/opentype/noto/NotoSansCJK-VF.ttf.ttc', 'Noto Sans CJK'],
 ];
 
